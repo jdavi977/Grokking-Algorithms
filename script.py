8aefb36f1bc8e49938434f28eb5ac13ef65ea755
Jawne3 <johnlowendavid@yahoo.ca>
2025-02-14 18:11:45 -0700

    Created a binary search

--- script.py
+++ script.py
@@ -1,3 +1,21 @@
+# Binary Search that returns the index of the item
+def binary_search(list, item):
+    low = 0
+    high = len(list) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+my_list = [1, 3, 5, 2, 9]   
+print(binary_search(my_list, 5))
+        
 
 # Finding the smallest element in an array.
 def findSmallest(arr):
